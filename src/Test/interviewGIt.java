a670f186f932f00e7a849c8b0387308b76fd0d36
Zaker8055 <pmzakerr@gmail.com>
2024-02-22 17:58:56 +0530

    changes for java 01

---
+++ src/Test/interviewGIt.java
@@ -0,0 +1,12 @@
+package Test;
+
+public class interviewGIt {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("My name is GIT am in");
+
+	}
+
+}
