ee907b320b8597b8e8500092d2a5f326b855ea50
Zaker8055 <pmzakerr@gmail.com>
2024-02-22 18:09:26 +0530

    checking

--- src/Test/interviewGIt.java
+++ src/Test/interviewGIt.java
@@ -6,6 +6,16 @@ public class interviewGIt {
 		// TODO Auto-generated method stub
 		
 		System.out.println("My name is GIT am in");
+		System.out.println("My name is GIT am in");
+
+		System.out.println("My name is GIT am in");
+
+		System.out.println("My name is GIT am in");
+
+		System.out.println("My name is GIT am in");
+
+		System.out.println("My name is GIT am in");
+
 
 	}
 
